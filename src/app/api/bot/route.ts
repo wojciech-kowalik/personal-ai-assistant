bb1553f057cac5a19c1a1ab3f4bf3a716e640157
Wojciech Kowalik <wojciech.kowalik@auto1.com>
2025-03-28 15:04:54 +0100

    fix: remove comments

--- src/app/api/bot/route.ts
+++ src/app/api/bot/route.ts
@@ -1,22 +1,3 @@
-// export const dynamic = "force-dynamic";
-
-// export const fetchCache = "force-no-store";
-
-// import { Bot, webhookCallback } from "grammy";
-
-// const token = process.env.TELEGRAM_BOT_TOKEN;
-
-// if (!token)
-//   throw new Error("TELEGRAM_BOT_TOKEN environment variable not found.");
-
-// const bot = new Bot(token);
-
-// bot.on("message:text", async (ctx) => {
-//   await ctx.reply(ctx.message.text);
-// });
-
-// export const POST = webhookCallback(bot, "std/http");
-
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 import { webhookCallback } from "grammy";
