9639e22a89116a5c244e3ca6430f206225c6d637
Wojciech Kowalik <wojciech.kowalik@auto1.com>
2025-03-28 15:45:18 +0100

    fix: post request for the api

--- src/app/api/bot/route.ts
+++ src/app/api/bot/route.ts
@@ -1,8 +1,5 @@
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
-import { webhookCallback } from "grammy";
-
 import TelegramService from "@/services/telegram.service";
+import { type NextRequest } from "next/server";
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
@@ -11,9 +8,20 @@ if (!token)
 
 const telegramService = new TelegramService(token);
 
-await telegramService.onMessage((ctx) => {
-  ctx.reply("Hello, world!");
+telegramService.onMessage(async (ctx) => {
   console.log("Received message:", ctx.message?.text);
+  console.log("Chat ID:", ctx.chat?.id);
 });
 
-export const POST = webhookCallback(telegramService.getBot(), "std/http");
+telegramService.startPolling();
+
+telegramService.sendMessage("7504626147", "Bot started successfully!");
+
+export async function POST(request: NextRequest): Promise<Response> {
+  const payload = await request.json().catch(() => ({}));
+  console.log("Received webhook:", payload);
+
+  telegramService.sendMessage("7504626147", payload?.text);
+
+  return new Response("OK");
+}
